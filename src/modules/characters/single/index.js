ed90cb4da5847414a11e285d0f0027b92c19a8f7
Daniel <91888300+LordHippopotamus@users.noreply.github.com>
2022-03-04 10:26:13 +0300

    fix(CharacterPage): use separated component

--- src/modules/characters/single/index.js
+++ src/modules/characters/single/index.js
@@ -1,16 +1,6 @@
-import {
-  Container,
-  Heading,
-  Image,
-  Table,
-  Tbody,
-  Thead,
-  Tr,
-  Th,
-  Td
-} from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import { useFetchCharacterById } from "./useFetchCharacterById";
+import SingleCharacter from "./SingleCharacter";
 
 const CharacterPage = () => {
   const params = useParams();
@@ -18,46 +8,7 @@ const CharacterPage = () => {
 
   const character = useFetchCharacterById(id);
 
-  return (
-    <Container my={8}>
-      <Heading>{character.name}</Heading>
-      <Image src={character.image} alt={character.name} w="100%" />
-      <Table mt={4}>
-        <Thead>
-          <Tr>
-            <Th>Property</Th>
-            <Th>Value</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          <Tr>
-            <Td>status</Td>
-            <Td>{character.status}</Td>
-          </Tr>
-          <Tr>
-            <Td>species</Td>
-            <Td>{character.species}</Td>
-          </Tr>
-          <Tr>
-            <Td>type</Td>
-            <Td>{character.type}</Td>
-          </Tr>
-          <Tr>
-            <Td>gender</Td>
-            <Td>{character.gender}</Td>
-          </Tr>
-          <Tr>
-            <Td>origin</Td>
-            <Td>{character.origin.name}</Td>
-          </Tr>
-          <Tr>
-            <Td>location</Td>
-            <Td>{character.location.name}</Td>
-          </Tr>
-        </Tbody>
-      </Table>
-    </Container>
-  );
+  return <SingleCharacter character={character} />;
 };
 
 export default CharacterPage;
