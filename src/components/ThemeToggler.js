4f8fd3fa4494251aa0794f199c116a750dbb4b55
Daniel <91888300+LordHippopotamus@users.noreply.github.com>
2022-03-04 13:04:21 +0300

    fix(ThemeToggler): invalid z-index

--- src/components/ThemeToggler.js
+++ src/components/ThemeToggler.js
@@ -1,27 +1,26 @@
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
-import { useColorMode, Button, Box } from "@chakra-ui/react";
+import { useColorMode, Button } from "@chakra-ui/react";
 
 const ThemeToggler = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Box zIndex={10} position="fixed" h="100vh" w="100%">
-      <Button
-        variant="solid"
-        bg={colorMode === "light" ? "gray.300" : "gray.700"}
-        _hover={{
-          bg: colorMode === "light" ? "gray.100" : "gray.900"
-        }}
-        position="absolute"
-        bottom={8}
-        right={8}
-        onClick={toggleColorMode}
-        h={12}
-        w={12}
-        borderRadius="full"
-      >
-        {colorMode === "light" ? <SunIcon /> : <MoonIcon />}
-      </Button>
-    </Box>
+    <Button
+      variant="solid"
+      bg={colorMode === "light" ? "gray.300" : "gray.700"}
+      _hover={{
+        bg: colorMode === "light" ? "gray.100" : "gray.900"
+      }}
+      position="fixed"
+      zIndex={10}
+      bottom={8}
+      right={8}
+      h={12}
+      w={12}
+      borderRadius="full"
+      onClick={toggleColorMode}
+    >
+      {colorMode === "light" ? <SunIcon /> : <MoonIcon />}
+    </Button>
   );
 };
 
