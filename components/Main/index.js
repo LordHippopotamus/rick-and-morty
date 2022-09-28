ec047c3bb9c932718e42f3d5d16e9804b99789cf
Daniel <91888300+LordHippopotamus@users.noreply.github.com>
2022-09-28 10:06:16 +0300

    feat: add Head component

--- components/Main/index.js
+++ components/Main/index.js
@@ -2,30 +2,40 @@ import { Box, Container, Grid } from '@mui/material';
 import Filters from './Filters';
 import Card from './Card';
 import Pagination from './Pagination';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-const Main = ({ fields, list, pages, button, path = '' }) => (
-  <Box my={4}>
-    <Box display={{ lg: 'flex' }}>
-      <Box display={{ xs: 'none', lg: 'block' }} flexBasis="24rem">
-        <Filters fields={fields} />
-      </Box>
-      <Container>
-        <Box display="flex" flexDirection="column" gap={4}>
-          <Box display={{ lg: 'none' }}>
-            <Filters fields={fields} button={button} path={path} />
+const Main = ({ fields, list, pages, button, path = '' }) => {
+  const { pathname } = useRouter();
+  return (
+    <>
+      <Head>
+        <title>R&M | {pathname[1].toUpperCase() + pathname.slice(2) + 's'}</title>
+      </Head>
+      <Box my={4}>
+        <Box display={{ lg: 'flex' }}>
+          <Box display={{ xs: 'none', lg: 'block' }} flexBasis="24rem">
+            <Filters fields={fields} />
           </Box>
-          <Grid container spacing={2}>
-            {list.map(el => (
-              <Grid item xs={12} sm={6} md={4} lg={3} key={el.id}>
-                <Card {...el} path={path} />
+          <Container>
+            <Box display="flex" flexDirection="column" gap={4}>
+              <Box display={{ lg: 'none' }}>
+                <Filters fields={fields} button={button} path={path} />
+              </Box>
+              <Grid container spacing={2}>
+                {list.map(el => (
+                  <Grid item xs={12} sm={6} md={4} lg={3} key={el.id}>
+                    <Card {...el} path={path} />
+                  </Grid>
+                ))}
               </Grid>
-            ))}
-          </Grid>
-          <Pagination pages={pages} path={path} />
+              <Pagination pages={pages} path={path} />
+            </Box>
+          </Container>
         </Box>
-      </Container>
-    </Box>
-  </Box>
-);
+      </Box>
+    </>
+  );
+};
 
 export default Main;
--- components/Single/index.js
+++ components/Single/index.js
@@ -1,4 +1,6 @@
 import { Box, Container, Grid } from '@mui/material';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { default as MyArray } from './Array';
 import Image from './Image';
 import Link from './Link';
@@ -20,41 +22,53 @@ const Single = ({
   episode,
   residents,
   characters,
-}) => (
-  <Box my={4}>
-    <Container>
-      <Grid container spacing={2}>
-        {image ? <Image image={image} alt={name} /> : null}
-        <Grid item xs={12} sm>
-          {name ? <Simple label="name" value={name} /> : null}
-          {status ? <Simple label="status" value={status} /> : null}
-          {species ? <Simple label="species" value={species} /> : null}
-          {type ? <Simple label="type" value={type} /> : null}
-          {gender ? <Simple label="gender" value={gender} /> : null}
-          {dimension ? <Simple label="dimension" value={dimension} /> : null}
-          {air_date ? <Simple label="air date" value={air_date} /> : null}
-          {episode && !Array.isArray(episode) ? <Simple label="episode" value={episode} /> : null}
+}) => {
+  const { pathname } = useRouter();
+  return (
+    <>
+      <Head>
+        <title>R&M | {name}</title>
+      </Head>
+      <Box my={4}>
+        <Container>
+          <Grid container spacing={2}>
+            {image ? <Image image={image} alt={name} /> : null}
+            <Grid item xs={12} sm>
+              {name ? <Simple label="name" value={name} /> : null}
+              {status ? <Simple label="status" value={status} /> : null}
+              {species ? <Simple label="species" value={species} /> : null}
+              {type ? <Simple label="type" value={type} /> : null}
+              {gender ? <Simple label="gender" value={gender} /> : null}
+              {dimension ? <Simple label="dimension" value={dimension} /> : null}
+              {air_date ? <Simple label="air date" value={air_date} /> : null}
+              {episode && !Array.isArray(episode) ? (
+                <Simple label="episode" value={episode} />
+              ) : null}
 
-          {origin ? (
-            <Link label="origin" url={'/location/' + findId(origin.url)} value={origin.name} />
-          ) : null}
-          {location ? (
-            <Link
-              label="location"
-              url={'/location/' + findId(location.url)}
-              value={location.name}
-            />
-          ) : null}
+              {origin ? (
+                <Link label="origin" url={'/location/' + findId(origin.url)} value={origin.name} />
+              ) : null}
+              {location ? (
+                <Link
+                  label="location"
+                  url={'/location/' + findId(location.url)}
+                  value={location.name}
+                />
+              ) : null}
 
-          {episode && Array.isArray(episode) ? (
-            <MyArray label="episodes" url="/episode/" array={episode} />
-          ) : null}
-          {residents ? <MyArray label="residents" url="/character/" array={residents} /> : null}
-          {characters ? <MyArray label="characters" url="/character/" array={characters} /> : null}
-        </Grid>
-      </Grid>
-    </Container>
-  </Box>
-);
+              {episode && Array.isArray(episode) ? (
+                <MyArray label="episodes" url="/episode/" array={episode} />
+              ) : null}
+              {residents ? <MyArray label="residents" url="/character/" array={residents} /> : null}
+              {characters ? (
+                <MyArray label="characters" url="/character/" array={characters} />
+              ) : null}
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </>
+  );
+};
 
 export default Single;
